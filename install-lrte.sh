159ff1551d15224f1de380c47ba58235da48da93
Ming Zhao <mzhao@luminatewireless.com>
2015-09-23 22:56:38 -0700

    Add script to install lrte. Enable sudo.

---
+++ install-lrte.sh
@@ -0,0 +1,13 @@
+RELEASE=v2.0_0
+for name in runtime headers; do
+    local package=lrtev2-${name}_1.0-0_amd64.deb
+    wget https://github.com/mzhaom/lrte/releases/download/${RELEASE}/${package}
+    sudo dpkg -i ${package}
+done
+
+GCC=lrtev2-crosstoolv2-gcc-4.9_1.0-8.223995svn_amd64.deb
+CLANG=lrtev2-crosstoolv2-clang-3.7_1.0-8.238804svn_amd64.deb
+for package in $GCC $CLANG; do
+    wget https://github.com/mzhaom/lrte/releases/download/${RELEASE}/${package}
+    sudo dpkg -i ${package}
+done
